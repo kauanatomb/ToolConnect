0566657673446162ce2056ec9c68174797293f8c
Patrick Bird <p_h_bird@yahoo.fr>
2023-05-30 15:54:46 +0200

    added some seed data in the tools table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+patrick = User.create first_name: "Patrick", last_name: "Bird", email: "pb@gmail.com", password: "password"
+
+require 'csv'
+
+CSV.foreach(Rails.root.join('db/tools.csv'), headers: true) do |row|
+  Tool.create!({
+    name: row[0],
+    address: row[1],
+    description: row[2],
+    availability: row[3],
+    user_id: patrick.id
+  })
+end
