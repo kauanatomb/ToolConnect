08751ee12331f0b69509ba422263dd1239434248
Yaya Rezai <yayarezai3@gmail.com>
2023-05-30 12:04:47 +0200

    some changes made in the tools

---
+++ db/migrate/20230530091236_create_tools.rb
@@ -0,0 +1,14 @@
+class CreateTools < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tools do |t|
+      t.string :name
+      t.string :description
+      t.string :photo
+      t.string :address
+      t.string :availability
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_145022) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_151306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "tool_id", null: false
+    t.string "information"
+    t.string "review"
+    t.date "rental_start"
+    t.date "rental_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tool_id"], name: "index_bookings_on_tool_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "tools", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "photo"
+    t.string "address"
+    t.string "availability"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tools_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_145022) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "tools"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "tools", "users"
 end
