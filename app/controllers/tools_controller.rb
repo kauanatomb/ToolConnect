e1c513a09292f0d1b995e09fbe9149749492ab6b
Yaya Rezai <yayarezai3@gmail.com>
2023-05-30 12:44:16 +0200

    Tools_controller is made and I have created for the tools_controller a index, show, new, create, edit, update and destry

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,49 @@
+class ToolsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = current_user.tools.build(tool_params)
+    if @tool.save
+      redirect_to @tool, notice: 'Tool was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @tool = Tool.find(params[:id])
+  end
+
+  def update
+    @tool = Tool.find(params[:id])
+    if @tool.update(tool_params)
+      redirect_to @tool, notice: 'Tool was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tool = Tool.find(params[:id])
+    @tool.destroy
+    redirect_to tools_path, notice: 'Tool was successfully deleted.'
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :description, :photo, :address, :availability)
+  end
+end
