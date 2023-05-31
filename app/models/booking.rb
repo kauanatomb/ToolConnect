180ab50a33f385137cb363f4aadf4b998712d077
Andreas Aichinger <aichingerandreas@hotmail.com>
2023-05-31 15:42:26 +0200

    enum implemented

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tool
   validates :rental_start, :rental_end, presence: true
+  enum status: [:pending, :accepted, :declined]
 end
