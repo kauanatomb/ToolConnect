5686b66a25c5ff35617851393ca1a5e5c4ea7d90
Yaya Rezai <yayarezai3@gmail.com>
2023-06-01 14:57:41 +0200

    changes made in the seeds but not pushed yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 require 'csv'
+require 'open-uri'
 
 Tool.destroy_all
 User.destroy_all
@@ -24,22 +25,8 @@ CSV.foreach(Rails.root.join('db/tools.csv'), headers: true) do |row|
     availability: row[3],
     user: User.all.sample
   })
-  tool.image.attach( io: File.open("app/assets/images/toolbox.png"), filename: "tools.png", content_type: "image/png")
+  # tool.image.attach( io: File.open("url.png"), filename: "tools.png", content_type: "image/png")
+  random_image = URI.open(Faker::LoremFlickr.image(size: "300x200", search_terms: ['tools', 'hammer']))
+  tool.image.attach( io: random_image, filename: "#{tool.name}.png", content_type: "image/png")
   tool.save
 end
-
-####
-# images = Dir.glob("app/assets/images/*.png")
-
-# CSV.foreach(Rails.root.join('db/tools.csv'), headers: true) do |row|
-#   tool = Tool.new({
-#     name: row[0],
-#     address: row[1],
-#     description: row[2],
-#     availability: row[3],
-#     user: User.all.sample
-#   })
-#   image = images.sample
-#   tool.image.attach(io: File.open(image), filename: File.basename(image), content_type: "image/png")
-#   tool.save
-# end
